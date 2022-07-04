c70e263b83c423c0bf96e399773a3e9dc8b07dd2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-04 18:05:44 +0900

    chore: add gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,13 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/index.tsx";
+import * as $0 from "./routes/api/class/index.ts";
+import * as $1 from "./routes/api/schema.ts";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/schema-org-gen/index.tsx";
+import * as $$0 from "./islands/Main.tsx";
+import * as $$1 from "./islands/Preview.tsx";
 
 const manifest = {
   routes: {
-    "./routes/index.tsx": $0,
+    "./routes/api/class/index.ts": $0,
+    "./routes/api/schema.ts": $1,
+    "./routes/index.tsx": $2,
+    "./routes/schema-org-gen/index.tsx": $3,
   },
-  islands: {},
+  islands: {
+    "./islands/Main.tsx": $$0,
+    "./islands/Preview.tsx": $$1,
+  },
   baseUrl: import.meta.url,
 };
 
