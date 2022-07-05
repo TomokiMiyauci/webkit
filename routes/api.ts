2141d90ad99235bbbf72fcb32d9eaac5c5e695c6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-06 02:07:51 +0900

    chore(routes): add debug

--- routes/api.ts
+++ routes/api.ts
@@ -2,7 +2,7 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
   GET() {
-    console.log(Deno);
+    console.log(Deno.readTextFile);
 
     return new Response("hello");
   },
