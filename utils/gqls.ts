9135456a4588315f9b9eec82955ae45a3b5d261e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-07 23:43:23 +0900

    chore(utils): add tiny GraphQL client

--- utils/gqls.ts
+++ utils/gqls.ts
@@ -8,7 +8,60 @@ export function gql([query]: TemplateStringsArray): string {
     .trim();
 }
 
-type Params = {};
+type json = string | number | boolean | null | { [k: string]: json } | json[];
+
+type Params = {
+  /** GraphQL query */
+  query: string;
+
+  /** GraphQL endpoint. */
+  endpoint: URL;
+};
+
+type Options = {
+  /** GraphQL variables. */
+  variables: json;
+};
 
 /** Tiny GraphQL fetch client. */
-export function gqlFetch<R>(params: Readonly<Params>) {}
+export async function gqlFetch<R extends Record<string, unknown>>(
+  { endpoint, query }: Readonly<Params>,
+  { variables }: Readonly<Partial<Options>> = {},
+  init?: RequestInit,
+): Promise<R> {
+  endpoint.searchParams.set("query", query);
+  if (variables) {
+    const varStr = JSON.stringify(variables);
+    endpoint.searchParams.set("variables", varStr);
+  }
+
+  const res = await fetch(endpoint, init);
+  return resolveResponse<R>(res);
+}
+
+type GqlLocation = { line: number; column: number };
+type GqlError = { message: string; locations: GqlLocation[] };
+
+type Result<T extends Record<string, unknown> = Record<string, unknown>> = {
+  data?: T;
+  errors: GqlError[];
+};
+
+export async function resolveResponse<R extends Record<string, unknown>>(
+  res: Response,
+): Promise<R> {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  const { data, errors } = await (res.json() as Promise<Result<R>>);
+
+  if (errors) {
+    const e = errors.map(({ message }) => Error(message));
+    throw new AggregateError(e, `Query result contains one or more errors.`);
+  }
+  if (!data) {
+    throw TypeError(`Query result has no "data" field.`);
+  }
+
+  return data;
+}
