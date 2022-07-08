b7b5e041fa3bc0f01fa4105d714c8d21479b0ede
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-07-09 02:39:00 +0900

    feat(graphql): use graphql http request validator

--- routes/graphql.ts
+++ routes/graphql.ts
@@ -1,49 +1,46 @@
-import { Handlers } from "$fresh/server.ts";
+import { Handler } from "$fresh/server.ts";
 import rootValue from "@/schemas/mod.ts";
 import { dirname, fromFileUrl, join } from "std/path/mod.ts";
 import { contentType } from "std/media_types/mod.ts";
 import { buildSchema, graphql } from "graphql";
 import { resolveErrorMsg } from "@/utils/errors.ts";
+import { validateRequest } from "@/utils/graphql_http_validates.ts";
 
 const fileUrl = fromFileUrl(import.meta.url);
 const filePath = join(dirname(fileUrl), "..", "schemas", "schema.graphql");
 const schemaData = await Deno.readTextFile(filePath);
 const schema = buildSchema(schemaData);
 
-export const handler: Handlers = {
-  async GET(req) {
-    const url = new URL(req.url);
-    const source = url.searchParams.get("query");
-    const variables = url.searchParams.get("variables");
-    const variableValues = variables ? JSON.parse(variables) : undefined;
+export const handler: Handler = async (req) => {
+  const result = await validateRequest(req);
+  if (!result[0]) {
+    return new Response(result[1].message, {
+      status: 400,
+    });
+  }
 
-    if (!source) {
-      const res = new Response(`"query" parameter is required.`, {
-        status: 400,
-      });
-      return res;
-    }
+  const { query: source, variableValues, operationName } = result[1];
 
-    try {
-      const result = await graphql({
-        schema,
-        source,
-        rootValue,
-        variableValues,
-      });
-      const res = new Response(JSON.stringify(result), {
-        headers: {
-          "content-type": contentType(".json"),
-          "cache-control": "max-age=60",
-        },
-      });
-      return res;
-    } catch (e) {
-      const msg = resolveErrorMsg(e);
-      const res = new Response(msg, {
-        status: 500,
-      });
-      return res;
-    }
-  },
+  try {
+    const result = await graphql({
+      schema,
+      source,
+      rootValue,
+      variableValues,
+      operationName,
+    });
+    const res = new Response(JSON.stringify(result), {
+      headers: {
+        "content-type": contentType(".json"),
+        "cache-control": "max-age=60",
+      },
+    });
+    return res;
+  } catch (e) {
+    const msg = resolveErrorMsg(e);
+    const res = new Response(msg, {
+      status: 500,
+    });
+    return res;
+  }
 };
